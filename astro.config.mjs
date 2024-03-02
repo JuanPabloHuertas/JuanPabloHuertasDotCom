da24a5daecf740fb0052aa8c65444dd54f27050d
JuanPabloHuertas <juanhuertas.dev@gmail.com>
2024-03-02 17:44:15 -0500

    Change files location

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://juanpablohuertas.com',
-    outDir: 'public',
-    publicDir: 'static',
+    site: 'https://juanpablohuertas.com'
 });
