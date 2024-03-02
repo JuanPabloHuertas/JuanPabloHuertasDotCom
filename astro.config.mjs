cbf79a5cf9e7bb2bf8d62786abd11693f843dbf4
JuanPabloHuertas <juanhuertas.dev@gmail.com>
2024-03-02 17:33:35 -0500

    Add files base - Astrojs

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://juanpablohuertas.com',
+    outDir: 'public',
+    publicDir: 'static',
+});
